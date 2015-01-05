04d4f7ad8ade9e9b9a567362be71cea6c760ba1d
kalavrouzi <niklausk@gmail.com>
2015-01-05 09:35:09 +0200

    Update select - Prepared Statement.php

--- select/select - Prepared Statement.php
+++ select/select - Prepared Statement.php
@@ -49,10 +49,6 @@ $stmt->close();
 // 						Example
 // ******************************************************* //
 
-$DBServer = 'db33.grserver.gr'; // e.g 'localhost' or '192.168.1.100'
-$DBUser   = 'foituseradmi';
-$DBPass   = '159spitaki823student';
-$DBName   = 'foititospitodbnewapp';
 
 $dbc = new mysqli($DBServer, $DBUser, $DBPass, $DBName);
 
